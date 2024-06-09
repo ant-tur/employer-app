71f514d1d32e12d1ae15037827193faf3313bd8e
Anton Tur <ant.tur85@gmail.com>
2024-06-09 13:38:38 +0300

    almost done

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,16 +1,40 @@
+import cn from 'classnames';
+
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = (props) => {
+	const { name, salary, onDelete, onToggleProp, increase, rise } = props;
+
 	return (
-		<li className='list-group-item d-flex justify-content-between'>
-			<span className='list-group-item-label'>John Smith</span>
-			<input type='text' className='list-group-item-input' defaultValue='1000$' />
+		<li
+			className={cn('list-group-item', 'd-flex', 'justify-content-between', {
+				increase,
+				like: rise,
+			})}
+		>
+			<span
+				onClick={onToggleProp}
+				data-toggle='rise'
+				className='list-group-item-label'
+			>
+				{name}
+			</span>
+			<input
+				type='text'
+				className='list-group-item-input'
+				defaultValue={salary + '$'}
+			/>
 			<div className='d-flex justify-content-center align-items-center'>
-				<button type='button' className='btn-cookie btn-sm '>
+				<button
+					type='button'
+					className='btn-cookie btn-sm '
+					onClick={onToggleProp}
+					data-toggle='increase'
+				>
 					<i className='fas fa-cookie'></i>
 				</button>
 
-				<button type='button' className='btn-trash btn-sm '>
+				<button type='button' className='btn-trash btn-sm ' onClick={onDelete}>
 					<i className='fas fa-trash'></i>
 				</button>
 				<i className='fas fa-star'></i>
---
+++ src/components/script.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const num = 5;
+console.log(num++);
